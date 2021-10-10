776c342b26863e781bce4875827f0a7a9f74a3d2
Abhishek Thory <abhithory@gmail.com>
2021-10-10 10:33:53 +0530

    Updates

---
+++ precache-manifest.e6cbb6d0f910460a5b7f0562b41f3df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9e1cd0adf0e41b6431f",
+    "url": "/Our_Todo_App/static/css/main.5c6a756f.chunk.css"
+  },
+  {
+    "revision": "a9e1cd0adf0e41b6431f",
+    "url": "/Our_Todo_App/static/js/main.a9e1cd0a.chunk.js"
+  },
+  {
+    "revision": "eaf6e0bb5b06dafd960d",
+    "url": "/Our_Todo_App/static/css/1.34c9c637.chunk.css"
+  },
+  {
+    "revision": "eaf6e0bb5b06dafd960d",
+    "url": "/Our_Todo_App/static/js/1.eaf6e0bb.chunk.js"
+  },
+  {
+    "revision": "e71cc06468b270534bbb",
+    "url": "/Our_Todo_App/static/js/runtime~main.e71cc064.js"
+  },
+  {
+    "revision": "8c58fe9d06039bc3ae3612fae81a3e15",
+    "url": "/Our_Todo_App/index.html"
+  }
+];
